0883d53240359f57117220e788d9040d1ea8cc71
chrishawkinsnz <chrishawkinsnz@gmail.com>
2018-11-21 22:52:31 +1300

    Initial Commit

---
+++ ScreenShotFriend/ScreenShotFriend.h
@@ -0,0 +1,19 @@
+//
+//  ScreenShotFriend.h
+//  ScreenShotFriend
+//
+//  Created by Chris Hawkins on 21/11/18.
+//  Copyright © 2018 Chris Hawkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScreenShotFriend.
+FOUNDATION_EXPORT double ScreenShotFriendVersionNumber;
+
+//! Project version string for ScreenShotFriend.
+FOUNDATION_EXPORT const unsigned char ScreenShotFriendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScreenShotFriend/PublicHeader.h>
+
+
